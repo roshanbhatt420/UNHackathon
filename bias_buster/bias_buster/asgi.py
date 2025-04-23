7ed0b6bb68030e6f7e61e8593fc403a1614feec5
Puspa Regmi <pusparegmi11b@gmail.com>
2025-04-24 00:58:24 +0545

    Add utility modules for bias analysis and attribute identification
    - Implemented `attribute_biasness_checker.py` for statistical analysis of column data to identify potential bias.
    - Created `csv_attributes_finder.py` to read CSV files and extract attribute names, with error handling for file operations.
    - Developed `sensitive_attributes_finder.py` to identify sensitive attributes in datasets using machine learning models.

--- bias_buster/bias_buster/asgi.py
+++ bias_buster/bias_buster/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for bais_buster project.
+ASGI config for bias_buster project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bais_buster.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bias_buster.settings')
 
 application = get_asgi_application()
---
+++ bias_buster/utils/attribute_biasness_checker.py
@@ -0,0 +1,7 @@
+'''
+Functionality: Performs statistical analysis on the column data to identify potential biasness.
+
+Input: Column data for testing biasness.
+
+Output: A dictionary containing the results of the biasness analysis.
+'''
