653bc95a7932eacf3ce0776bfa51fb661eb8e824
Puspa Regmi <pusparegmi11b@gmail.com>
2025-04-24 00:50:25 +0545

    Restore CSV attribute finder utility and add sensitive attributes identification module

---
+++ bais_buster/utils/sensitive_attributes_finder.py
@@ -0,0 +1,9 @@
+'''
+This module provides functionality to identify sensitive attributes in a dataset using a machine learning model.
+
+Inputs:
+    - A list of attribute names (column names) from a CSV file.
+
+Outputs:
+    - A list of detected sensitive attributes.
+'''
